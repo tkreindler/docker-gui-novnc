41e6cb14662255bab8b9722b0fd5aa68ba3b4424
tkreind <tristankreindler@gmail.com>
2021-03-30 13:29:17 -0400

    Switched to run and stop scripts

--- run.sh
+++ run.sh
@@ -39,6 +39,13 @@ fi
 
 echo ""
 
+# check if process is running
+docker ps | grep massif-visualizer-novnc > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Container is already running, use ./stop.sh to stop it"
+	exit 1
+fi
+
 $SUDO docker run -d --restart=always -v $1:/data -p 127.0.0.1:8087:8080 -e USERID=$UID -e GROUPID=$GID --name=massif-visualizer-novnc tkreind/massif-visualizer-novnc:latest
 
 if [ $? -ne 0 ]; then
@@ -46,12 +53,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Hosting massif-visualizer with files from $1 at http://localhost:8087"
-echo "Press control C to quit"
+echo "Run ./stop.sh to quit"
 
 # open the browser after a delay
 sleep 2 && $OPEN http://localhost:8087 > /dev/null &
-
-# reattach to the process so we can kill it
-docker attach massif-visualizer-novnc
-
-$SUDO docker rm massif-visualizer-novnc
---
+++ stop.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+# default to not use sudo
+SUDO=""
+
+# check docker permissions on Linux
+uname -a | grep Linux > /dev/null
+if [ $? -eq 0 ]; then
+	# if not root
+	if [ $UID -ne 0 ]; then
+		# check if user is in docker group
+		id $UID | grep docker > /dev/null
+		if [ $? -ne 0 ]; then
+			echo "User doesn't have permissions to use docker"
+			echo "Asking for sudo password"
+			SUDO=sudo
+		fi
+	fi
+fi
+
+# check if process is running
+docker ps | grep massif-visualizer-novnc > /dev/null
+if [ $? -eq 0 ]; then
+    $SUDO docker stop massif-visualizer-novnc > /dev/null
+    $SUDO docker rm massif-visualizer-novnc > /dev/null
+else
+    echo "Container was not running, use ./run.sh to start"
+fi
